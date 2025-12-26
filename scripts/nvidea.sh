44f7bb6994f72537110569ef746c1e61f7b34f34
aleksandrsoldatov <alex.soldatov.home@gmail.com>
2025-12-26 15:59:45 +0300

    requipments changed

--- scripts/nvidea.sh
+++ scripts/nvidea.sh
@@ -1,26 +1,77 @@
-!/bin/bash
+#!/bin/bash
+
+'''
+Что делает этот скрипт?
+
+Он обновляет систему, определяя тип видеокарты по ее DeviceID и устанавливая ядро Linux 6-й версии, 
+которое нужно для A100/H100 серии на Ubuntu 22.04;
+
+Устанавливает рекомендованные Ubuntu драйвера на вашу видеокарту;
+
+Подключает репозитории Nvidia и устанавливает CUDA;
+
+Прописывает необходимые переменные окружения для ее работы, попутно восстанавливая пути
+ к /bin и /usr/bin, которые вы гарантированно поломаете, если будете следовать официальной
+ инструкции;
+
+Если у вас установлен Docker, то добавляет поддержку GPU в контейнеры.
+
+Скрипт должен корректно отрабатывать на любых видеокартах старше 8xx версии и до последних 
+моделей. Проверен на Tesla T4, GTX 1080, A4000, A5000, RTX 4090 и H100.
+
+Сохраните его у себя в домашней директории как nvidia_install.sh, не забудьте задать через 
+chmod +x nvidia_install.sh права на запуск и запустите через sudo nvidia_install.sh. 
+Через 5-10 минут (в зависимости от скорости вашего интернета) вы получите работающую систему, 
+на которой можно дальше экспериментировать с нейросетями.
+
+'''
+
+# Update and upgrade the system using apt
+sudo apt update
+sudo apt upgrade -y
+
+#Check Ubuntu 22.04 and update kernel
+lsb_release=$(lsb_release -a | grep "22.04")
+if [[ -n "$lsb_release" ]]; then
+
+    # Check if there's a video card with Nvidia (10de) H100 model (23xx)
+    lspci_output=$(lspci -nnk | awk '/\[10de:23[0-9a-f]{2}\]/ {print $0}')
+    if [[ -n "$lspci_output" ]]; then
+        echo "A100 detected"
+        # If yes install the necessary kernel package
+        sudo apt install -y linux-generic-hwe-22.04
+    fi
+
+    # Check if there's a video card with Nvidia (10de) A100 model (20xx)
+    lspci_output=$(lspci -nnk | awk '/\[10de:20[0-9a-f]{2}\]/ {print $0}')
+    if [[ -n "$lspci_output" ]]; then
+        echo "A100 detected"
+        # If yes install the necessary kernel package
+        sudo apt install -y linux-generic-hwe-22.04
+    fi
+fi
 
 # Install Ubuntu drivers common package
-apt install ubuntu-drivers-common -y
+sudo apt install ubuntu-drivers-common -y
 
 recommended_driver=$(ubuntu-drivers devices | grep 'nvidia' | cut -d ',' -f 1 | grep 'recommended')
 package_name=$(echo $recommended_driver | awk '{print $3}')
-apt install $package_name -y
+sudo apt install $package_name -y
 
 # Install GCC compiler for CUDA install
-apt install gcc -y
+sudo apt install gcc -y
 
 # Get the release version of Ubuntu
 RELEASE_VERSION=$(lsb_release -rs | sed 's/\([0-9]\+\)\.\([0-9]\+\)/\1\2/')
 
 # Download and install CUDA package for Ubuntu
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${RELEASE_VERSION}/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
 # Update and upgrade the system again to ensure all packages are installed correctly
-apt update
-apt install cuda -y
-apt install nvidia-cuda-toolkit -y
+sudo apt update
+sudo apt install cuda -y
+sudo apt install nvidia-cuda-toolkit -y
 
 # Add PATH and LD_LIBRARY_PATH environment variables for CUDA in .bashrc file
 echo 'export PATH="/usr/bin:/bin:$PATH/usr/local/cuda/bin\${PATH:+:\${PATH}}"' >> ~/.bashrc
@@ -31,8 +82,8 @@ source ~/.bashrc
 
 if command -v docker &> /dev/null; then
   echo "Docker is installed."
-  apt install -y nvidia-docker2
-  systemctl restart docker
+  sudo apt install -y nvidia-docker2
+  sudo systemctl restart docker
 else
   echo "Docker is not installed."
 fi
